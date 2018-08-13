59db69ad64f7824ec0b7a1e542548e305197d0b0
勇太 松永 <m1m17r22@oit.ac.jp>
2018-08-13 20:21:02 +0900

    upgrade

--- object/object.go
+++ object/object.go
@@ -15,6 +15,7 @@ const (
 	RETURN_VALUE_OBJ	= "RETURN_VALUE"
 	ERROR_OBJ			= "ERROR"
 	FUNCTION_OBJ		= "FUNCTION"
+	STRING_OBJ			= "STRING"
 )
 
 type Object interface {
@@ -94,4 +95,14 @@ func (f *Function) Inspect() string {
 	out.WriteString("\n}")
 
 	return out.String()
+}
+
+type String struct {
+	Value	string
+}
+func (s *String) Type() ObjectType {
+	return STRING_OBJ
+}
+func (s *String) Inspect() string {
+	return s.Value
 }
