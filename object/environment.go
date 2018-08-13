8f29ac1e267b47a0d957ec7cecfceff754737376
勇太 松永 <m1m17r22@oit.ac.jp>
2018-08-13 18:35:31 +0900

    upgrade

--- object/environment.go
+++ object/environment.go
@@ -5,12 +5,22 @@ func NewEnvironment() *Environment {
 	return &Environment{store: s}
 }
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 type Environment struct {
-	store map[string]Object
+	store	map[string]Object
+	outer	*Environment
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
